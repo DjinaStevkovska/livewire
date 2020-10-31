504e1e6cfe8103d5d211a7c0b901faef2f1f31d2
DjinaStevkovska <stevkovskadjina@yahoo.com>
2020-10-31 15:48:39 +0100

    contact form

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact Us Form</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<livewire:styles />
+
+</head>
+<body>	
+	<div class="container">
+
+		<livewire:scripts />
+		
+		<br><br><hr><br><br>
+
+		<livewire:contact-us />
+
+		<br><br><hr><br><br>
+		
+		{{-- <livewire:contact-us /> --}}
+
+		<br><br><hr><br><br>
+
+
+	</div>
+
+	
+</body>
+</html>
